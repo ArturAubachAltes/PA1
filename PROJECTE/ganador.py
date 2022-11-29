54fc336a1a9877d89802f27b6c808b8eb8568d90
3lsesar <119434908+3lsesar@users.noreply.github.com>
2022-11-29 18:25:45 +0100

    Create ganador.py

---
+++ PROJECTE/ganador.py
@@ -0,0 +1,7 @@
+for j in range(0, len(M)-1):
+  for i in range(0, len(M)-2):
+    a = True if M[j][i] == M[j][i+1] else False
+    b = True if M[j][i] == M[j+1][i] else False
+    c = True if M[j][i] == M[j+1][i+1] else False
+    d = True if 
+  'end' if a == True or b == True or c == True or d == True
