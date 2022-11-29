094232a1403b92ff742a18a1ad9bfba6e3a4e2ea
ArturAubachAltes <106880365+ArturAubachAltes@users.noreply.github.com>
2022-11-29 18:31:48 +0100

    Update ganador.py

--- PROJECTE/ganador.py
+++ PROJECTE/ganador.py
@@ -5,3 +5,6 @@ for j in range(0, len(M)-1):
     c = c and True if M[j][i] == M[j+1][i+1] else False
     d = d True if 
   'end' if a == True or b == True or c == True or d == True
+  
+  M=M_4X4 = [[1, 1, 2, 2], [2, 2, 2, 2], [1, 1, 1, 1], [1, 0, 0, 0]]
+  Modo_de_juego = 3
