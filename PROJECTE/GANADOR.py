8a2554007fef61620c0a45981dba16cf59194366
3lsesar <119434908+3lsesar@users.noreply.github.com>
2023-01-03 14:16:30 +0100

    SOY MUY FELIZ
    6H haciendo el dificl e intentado arreglarlo, pongo 15 contadores diferentes y funciona. No tengo palabras para describir esta sensacion de lucided que ha sido escribir el codigo que me gano una noche de resaca en media hora y a la primera (gracias Violeta por ponerme el print porque me iba a suicidar)

---
+++ PROJECTE/GANADOR.py
@@ -0,0 +1,41 @@
+
+M = [[0,1,1],[2,1,2],[1,2,2]]
+mode = 3
+
+
+def ganador(M,mode):
+	for jugador in range(1,2):
+		contadorcolumna = 1
+		contadorfila = 1
+		contadordiagp = 1
+		contadordiagn = 1
+		for fila in range(mode-1):
+			for columna in range(mode-1):
+				if M[fila][columna+1] == M[fila][columna] and jugador == M[fila][columna]:
+					contadorcolumna +=1 
+					if contadorcolumna == mode:
+						return jugador 
+					
+				elif M[fila+1][columna] == M[fila][columna] and jugador == M[fila][columna]:
+					contadorfila += 1
+					if contadorfila == mode:
+						return jugador
+					
+				elif M[fila+1][columna+1] == M[fila][columna] and jugador == M[fila][columna]:
+					contadordiagp += 1
+					if contadordiagp == mode:
+						return jugador
+					
+				elif M[fila][len(M)-1-columna] == M[fila+1][len(M)-2-columna] and jugador == M[fila][len(M)-1-columna]:
+					contadordiagn += 1
+					if contadordiagn == mode:
+						return jugador
+						
+						
+print(ganador(M,mode))
+					
+				
+				
+				
+				
+	
